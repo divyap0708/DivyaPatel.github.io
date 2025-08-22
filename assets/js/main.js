61c7eea1c0b9ad0ecd5e9461b60908d4ccf35045
Divya Patel <divyapatel7893@gmail.com>
2025-08-22 19:04:06 -0400

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -282,6 +282,14 @@
 				});
 				});
 
+				document.addEventListener("scroll", function() {
+					const nav = document.getElementById("nav");
+					if (window.scrollY > 50) {
+					nav.classList.add("scrolled");
+					} else {
+					nav.classList.remove("scrolled");
+					}
+				});
 
 })(jQuery);
 
